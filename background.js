3386c05133b11dfe204d301ae771c533bec7e7e5
NAMANIND <namanrai309@gmail.com>
2023-05-30 00:33:30 +0530

    new

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
-let isFormFilled = false;
-
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (!isFormFilled && changeInfo.status === "complete" && tab.url.startsWith("http://192.168.")) {
-    chrome.tabs.sendMessage(tabId, {
-      action: "fillForm"
-    });
-    
-    isFormFilled = true; // Set the flag to true to indicate that the form has been filled once
-  }
-});
+    if (changeInfo.status === "complete" && tab.url.startsWith("http://192.168.")) {
+      chrome.tabs.sendMessage(tabId, {
+        action: "fillForm"
+      });
+    }
+  });
+  
--- content.js
+++ content.js
@@ -25,7 +25,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     chrome.storage.local.get(["username", "password","redirectUrl"], function(result) {
       var storedUsername = result.username || "";
       var storedPassword = result.password || "";
-      var storedreURl = result.redirectUrl || "www.youtube.com/";
+      // var storedreURl = result.redirectUrl || "www.youtube.com/";
 
 
       
@@ -34,34 +34,35 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         passwordInput.value = storedPassword;
         // reUrlInput.value = storedreURl;
     
-      
-        
-        function openNewTab(url) {
-          if (!url.startsWith('http://') && !url.startsWith('https://')) {
-            url = 'https://' + url;
-          }
-          if(window.location.href.includes("/keepalive")){
-            window.open(url, '_blank');
-          }
-         
-        }
-
         var form = usernameInput.closest('form');
         if (form) {
           form.submit();
-          
         }
-        
-        openNewTab(storedreURl );
-      
-        
-      }
+      } 
     });
   }
-  
+
+    
+  chrome.storage.local.get(["redirectUrl"], function(result) {
+ 
+    var storedreURl = result.redirectUrl || "www.youtube.com/";
+
+  function openNewTab(url) {
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      url = 'https://' + url;
+    }
+    window.open(url, '_blank');
+  }
+
+  if (window.location.href.includes("keepalive")) {
+    openNewTab(storedreURl);
+  }
+
+
+});
 
 
   console.log("Made by Naman Rai ♥");
-    console.log("Follow me on github: github.com/NAMANIND");
+  console.log("Follow me on github: github.com/NAMANIND");
   
 
