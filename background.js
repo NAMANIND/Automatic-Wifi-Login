033676b72f1cd0b963460ea843d9deb50e016c04
NAMANIND <namanrai309@gmail.com>
2023-05-29 23:19:25 +0530

    change

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
+let isFormFilled = false;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete" && tab.url.startsWith("http://192.168.")) {
-      chrome.tabs.sendMessage(tabId, {
-        action: "fillForm"
-      });
-    }
-  });
-  
+  if (!isFormFilled && changeInfo.status === "complete" && tab.url.startsWith("http://192.168.")) {
+    chrome.tabs.sendMessage(tabId, {
+      action: "fillForm"
+    });
+    
+    isFormFilled = true; // Set the flag to true to indicate that the form has been filled once
+  }
+});
